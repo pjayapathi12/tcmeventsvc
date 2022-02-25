5ed17d7f7106f85827c9bb350a3ece9d2a3388a9
unknown <pradeep.sarathi@gmail.com>
2022-02-25 15:24:43 +0000

    project setup

---
+++ src/main/java/com/cloudathon/cloudathondemo/CloudathondemoApplication.java
@@ -0,0 +1,13 @@
+package com.cloudathon.cloudathondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudathondemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudathondemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cloudathon/cloudathondemo/DemoController.java
@@ -0,0 +1,32 @@
+package com.cloudathon.cloudathondemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DemoController {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    @PostMapping("/employee")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return repository.save(employee);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test Successful with Azure spring boot web app deployment and setup";
+    }
+
+}
