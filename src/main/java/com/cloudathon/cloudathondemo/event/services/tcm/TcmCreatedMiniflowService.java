7c35bb7235ecbfa351a524151ba6793cf6809460
unknown <pradeep.sarathi@gmail.com>
2022-02-28 18:00:56 +0000

    change resourceloader file to inputstream

--- src/main/java/com/cloudathon/cloudathondemo/event/services/tcm/TcmCreatedMiniflowService.java
+++ src/main/java/com/cloudathon/cloudathondemo/event/services/tcm/TcmCreatedMiniflowService.java
@@ -3,17 +3,14 @@ package com.cloudathon.cloudathondemo.event.services.tcm;
 import com.cloudathon.cloudathondemo.TcmEventProducer;
 import com.cloudathon.cloudathondemo.event.interfaces.TcmEventFlowInterface;
 import com.cloudathon.cloudathondemo.models.TcmEvent;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -36,14 +33,20 @@ public class TcmCreatedMiniflowService implements TcmEventFlowInterface {
 
         // 1. get data from mock file
         Resource resource = resourceLoader.getResource("classpath:errorstats-1.json");
+        InputStream inputStream = null;
+
         try {
-            File file = resource.getFile();
-            List<TcmEvent.ErrorStat> errorStat = objectMapper.readValue(file, new TypeReference<List<TcmEvent.ErrorStat>>() {
+            inputStream = resource.getInputStream();
+            List<TcmEvent.ErrorStat> errorStat = objectMapper.readValue(inputStream, new TypeReference<List<TcmEvent.ErrorStat>>() {
             });
             log.info("error stats retrieved from file ==> {}", errorStat);
             tcmEvent.getResources().forEach(x -> x.setErrorStats(errorStat) ); // set each resource with same errorstat
         } catch (Exception e) {
             log.info("error encountered while trying to open file");
+        }
+        finally {
+            if (inputStream != null)
+                inputStream.close();
         }
 
         // 2. publish TcmResourcesEnriched event to bus
